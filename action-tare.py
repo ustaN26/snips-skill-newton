1562b705f226af41318f59542c3a52e37a31cf0f
jrimet <johan.rimet@preciamolen.fr>
2019-07-25 10:26:33 +0200

    3

--- action-tare.py
+++ action-tare.py
@@ -7,7 +7,8 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
 def intent_received(hermes, intent_message):
-	hermes.publish_end_session(intent_message.session_id, "tared successfully")
+	if intent_message.intent.intent_name == 'ustaN:tare':
+		hermes.publish_end_session(intent_message.session_id, "tared successfully")
 
 with Hermes(MQTT_ADDR) as h:
 	h.subscribe_intents(intent_received).start()
