6629c13325cb56510bbfd36cc617015a4931dc77
jrimet <johan.rimet@preciamolen.fr>
2019-07-25 17:18:22 +0200

    test

--- action-demande-poid.py
+++ action-demande-poid.py
@@ -9,7 +9,7 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 def intent_received(hermes, intent_message):
 	print(intent_message.intent.intent_name)
-	if intent_message.intent.intent_name == 'ustaN:demandepoid':
+	if intent_message.intent.intent_name == 'ustaN:demandepoids':
 		try:
 			ser = serial.Serial(
 				port='/dev/ttyACM0',
@@ -17,14 +17,12 @@ def intent_received(hermes, intent_message):
 			)
 			ser.write(serial.to_bytes([0x01,0x09,0x30,0x30,0x05,0x30,0x31,0x4C,0x0D,0x0A]))
 			ser.flush()
-			out = ''
-			while ser.inWaiting() > 0:
-				out += ser.read(1)
+			out = ser.read(14)
 			ser.close()
-			hermes.publish_end_session(intent_message.session_id, "le poid brut est de "+str(out))
+			hermes.publish_end_session(intent_message.session_id, "le poids brut est de "+str(out))
 		except:
 			ser.close()
-			hermes.publish_end_session(intent_message.session_id, "erreur lecture poid")
+			hermes.publish_end_session(intent_message.session_id, "erreur lecture poids")
 
 with Hermes(MQTT_ADDR) as h:
 	h.subscribe_intents(intent_received).start()
