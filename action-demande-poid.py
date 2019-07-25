fddc7ce5d39b4ad92ffdbce4cebb2e9c44edc459
jrimet <johan.rimet@preciamolen.fr>
2019-07-25 17:01:54 +0200

    ajout action-demande-poid.py

---
+++ action-demande-poid.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from hermes_python.hermes import Hermes
+import serial
+
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+
+def intent_received(hermes, intent_message):
+	print(intent_message.intent.intent_name)
+	if intent_message.intent.intent_name == 'ustaN:demandepoid':
+		try:
+			ser = serial.Serial(
+				port='/dev/ttyACM0',
+				baudrate = 9600
+			)
+			ser.write(serial.to_bytes([0x01,0x09,0x30,0x30,0x05,0x30,0x31,0x4C,0x0D,0x0A]))
+			ser.flush()
+			out = ''
+			while ser.inWaiting() > 0:
+				out += ser.read(1)
+			ser.close()
+			hermes.publish_end_session(intent_message.session_id, "le poid brut est de "+str(out))
+		except:
+			ser.close()
+			hermes.publish_end_session(intent_message.session_id, "erreur lecture poid")
+
+with Hermes(MQTT_ADDR) as h:
+	h.subscribe_intents(intent_received).start()
