aa3996b1134f4d0117459a4e472e0e51d5314a55
jrimet <johan.rimet@preciamolen.fr>
2019-07-26 10:19:36 +0200

    3

--- action-demande-poids.py
+++ action-demande-poids.py
@@ -29,13 +29,12 @@ def intent_received(hermes, intent_message):
 				baudrate = 9600
 			)
 			ser.write(serial.to_bytes([0x01,0x09,0x30,0x30,0x05,0x30,0x31,0x4C,0x0D,0x0A]))
-			ser.flush()
 			time.sleep(0.5)
 			out = ser.read()
 			out = verbalise_unite(out)
 			out = out.replace("b'\x01\x0201"," ")
 			ser.close()
-			hermes.publish_end_session(intent_message.session_id, "le poids brut est de "+str(out))
+			hermes.publish_end_session(intent_message.session_id, "le poids brut est de "+out)
 		except:
 			ser.close()
 			hermes.publish_end_session(intent_message.session_id, "erreur lecture poids")
